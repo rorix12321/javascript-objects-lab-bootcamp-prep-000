b8bab52c1370ab0724d591bc9d922e8f43c8a391
Ricky Liu <rorix12321@gmail.com>
2018-06-14 06:56:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var recipes = {};
+<<<<<<< HEAD
 function updateObjectWithKeyAndValue(object,key,value) {
   var newrecipes = Object.assign({}, object, {[key]: value});
   return newrecipes;
 }
+=======
+
+>>>>>>> 779b0ad1f1428f1c8674fa12bb7e9131d3bd84be
 function destructivelyUpdateObjectWithKeyAndValue(object,key,value) {
   object[key] = value
   return object;
 }
+<<<<<<< HEAD
 function deleteFromObjectByKey(object,key) {
   var newrecipes = Object.assign({}, object)
   delete newrecipes[key];
@@ -16,3 +21,5 @@ function destructivelyDeleteFromObjectByKey(object,key) {
   delete object[key];
   return object;
 }
+=======
+>>>>>>> 779b0ad1f1428f1c8674fa12bb7e9131d3bd84be
